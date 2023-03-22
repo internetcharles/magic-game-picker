2242fafa1205c2b0fbd9f9cc0d5c6abb621d04a1
Charlie Smith <internetcharles1@gmail.com>
2023-03-22 13:20:25 -0700

    Jesus

--- src/Constants/genres.tsx
+++ src/Constants/genres.tsx
@@ -1,4 +1,226 @@
-import masterWebGameList from '../Data/mobygames/moby-web-paths.json'
-import { Game, GameByTagList, GameList } from '../Interfaces/GameInterface'
+import actionRPG from '../Data/mobygames/ps2/genres/action rpg.json'
+import action from '../Data/mobygames/ps2/genres/action.json'
+import adult from '../Data/mobygames/ps2/genres/adult.json'
+import adventure from '../Data/mobygames/ps2/genres/adventure.json'
+import africa from '../Data/mobygames/ps2/genres/africa.json'
+import anime from '../Data/mobygames/ps2/genres/anime.json'
+import arcade from '../Data/mobygames/ps2/genres/arcade.json'
+import asia from '../Data/mobygames/ps2/genres/asia.json'
+import baseball from '../Data/mobygames/ps2/genres/baseball.json'
+import basketball from '../Data/mobygames/ps2/genres/basketball.json'
+import beatEmUp from '../Data/mobygames/ps2/genres/beat em off.json'
+import bicycle from '../Data/mobygames/ps2/genres/bicycle.json'
+import boardGame from '../Data/mobygames/ps2/genres/board game.json'
+import boat from '../Data/mobygames/ps2/genres/boat type game.json'
+import bowling from '../Data/mobygames/ps2/genres/bowling.json'
+import boxing from '../Data/mobygames/ps2/genres/boxing.json'
+import businessSim from '../Data/mobygames/ps2/genres/business sim.json'
+import cardAndTile from '../Data/mobygames/ps2/genres/card & tile.json'
+import coldWar from '../Data/mobygames/ps2/genres/cold war.json'
+import comedy from '../Data/mobygames/ps2/genres/comedy.json'
+import compilation from '../Data/mobygames/ps2/genres/compilation.json'
+import cricket from '../Data/mobygames/ps2/genres/cricket.json'
+import crime from '../Data/mobygames/ps2/genres/crime.json'
+import cyberpunk from '../Data/mobygames/ps2/genres/cyberpunk.json'
+import datingSimulation from '../Data/mobygames/ps2/genres/dating simulation.json'
+import detectiveAndMystery from '../Data/mobygames/ps2/genres/detective & mystery.json'
+import educational from '../Data/mobygames/ps2/genres/educational.json'
+import europe from '../Data/mobygames/ps2/genres/europe.json'
+import fallingBlockPuzzle from '../Data/mobygames/ps2/genres/falling block puzzle.json'
+import fantasy from '../Data/mobygames/ps2/genres/fantasy.json'
+import fighting from '../Data/mobygames/ps2/genres/fighting.json'
+import firstPerson from '../Data/mobygames/ps2/genres/first person.json'
+import fishing from '../Data/mobygames/ps2/genres/fishing.json'
+import flight from '../Data/mobygames/ps2/genres/flight.json'
+import football from '../Data/mobygames/ps2/genres/football.json'
+import FMV from '../Data/mobygames/ps2/genres/full motion video.json'
+import soccer from '../Data/mobygames/ps2/genres/futbol.json'
+import gambling from '../Data/mobygames/ps2/genres/gambling.json'
+import giantRobot from '../Data/mobygames/ps2/genres/giant robot.json'
+import golf from '../Data/mobygames/ps2/genres/golf.json'
+import graphicAdventure from '../Data/mobygames/ps2/genres/graphic adventure.json'
+import hackAndSlash from '../Data/mobygames/ps2/genres/hack & slash.json'
+import helicopter from '../Data/mobygames/ps2/genres/helicopter.json'
+import historical from '../Data/mobygames/ps2/genres/historical events.json'
+import hockey from '../Data/mobygames/ps2/genres/hockey.json'
+import horror from '../Data/mobygames/ps2/genres/horror.json'
+import horse from '../Data/mobygames/ps2/genres/horse.json'
+import hovercraft from '../Data/mobygames/ps2/genres/hovercraft.json'
+import hunting from '../Data/mobygames/ps2/genres/hunting.json'
+import japanese from '../Data/mobygames/ps2/genres/japanese-style adventure.json'
+import lifeSim from '../Data/mobygames/ps2/genres/life sim.json'
+import martialArts from '../Data/mobygames/ps2/genres/martial arts.json'
+import meditation from '../Data/mobygames/ps2/genres/meditative & zen.json'
+import middleEast from '../Data/mobygames/ps2/genres/middle east.json'
+import miniGames from '../Data/mobygames/ps2/genres/mini-games.json'
+import motorcycle from '../Data/mobygames/ps2/genres/motorcycle.json'
+import musicRhythm from '../Data/mobygames/ps2/genres/music & rhythm.json'
+import northAmerica from '../Data/mobygames/ps2/genres/north america.json'
+import oceania from '../Data/mobygames/ps2/genres/oceania.json'
+import offRoading from '../Data/mobygames/ps2/genres/off-roading.json'
+import olympicAndMixedSports from '../Data/mobygames/ps2/genres/olympic & mixed sports.json'
+import paddleAndPong from '../Data/mobygames/ps2/genres/paddle & pong.json'
+import partyGame from '../Data/mobygames/ps2/genres/party game.json'
+import pirate from '../Data/mobygames/ps2/genres/pirate.json'
+import platform from '../Data/mobygames/ps2/genres/platform.json'
+import pointAndSelect from '../Data/mobygames/ps2/genres/point and select.json'
+import pool from '../Data/mobygames/ps2/genres/pool & snooker.json'
+import postApocalyptic from '../Data/mobygames/ps2/genres/post-apocalyptic.json'
+import puzzleElements from '../Data/mobygames/ps2/genres/puzzle elements.json'
+import Puzzle from '../Data/mobygames/ps2/genres/puzzle.json'
+import quickTimeEvents from '../Data/mobygames/ps2/genres/quick time events.json'
+import racingAndDriving from '../Data/mobygames/ps2/genres/racing & driving.json'
+import railShooter from '../Data/mobygames/ps2/genres/rail shooter.json'
+import realTimeStrategy from '../Data/mobygames/ps2/genres/real-time strategy.json'
+import romance from '../Data/mobygames/ps2/genres/romance.json'
+import RPGElements from '../Data/mobygames/ps2/genres/rpg elements.json'
+import RPG from '../Data/mobygames/ps2/genres/rpg.json'
+import rugby from '../Data/mobygames/ps2/genres/rugby.json'
+import sandboxAndOpenWorld from '../Data/mobygames/ps2/genres/sandbox & open world.json'
+import sciFi from '../Data/mobygames/ps2/genres/sci-fi.json'
+import shooter from '../Data/mobygames/ps2/genres/shooter.json'
+import simulation from '../Data/mobygames/ps2/genres/simulation.json'
+import skateboarding from '../Data/mobygames/ps2/genres/skateboarding.json'
+import spaceFlight from '../Data/mobygames/ps2/genres/space flight.json'
+import specificTimePeriod from '../Data/mobygames/ps2/genres/specific time period.json'
+import sports from '../Data/mobygames/ps2/genres/sports.json'
+import spy from '../Data/mobygames/ps2/genres/spy.json'
+import stealth from '../Data/mobygames/ps2/genres/stealth.json'
+import steampunk from '../Data/mobygames/ps2/genres/steampunk.json'
+import strategy from '../Data/mobygames/ps2/genres/strategy.json'
+import streetRacing from '../Data/mobygames/ps2/genres/street racing.json'
+import survivalHorror from '../Data/mobygames/ps2/genres/survival horror.json'
+import tacticalRPG from '../Data/mobygames/ps2/genres/tactical rpg.json'
+import tacticalShooter from '../Data/mobygames/ps2/genres/tactical shooter.json'
+import tank from '../Data/mobygames/ps2/genres/tank.json'
+import tennis from '../Data/mobygames/ps2/genres/tennis.json'
+import text from '../Data/mobygames/ps2/genres/text.json'
+import thirdPerson from '../Data/mobygames/ps2/genres/third person.json'
+import tileMatchingPuzzle from '../Data/mobygames/ps2/genres/tile matching puzzle.json'
+import trackRacing from '../Data/mobygames/ps2/genres/track racing.json'
+import tradingAndCollectible from '../Data/mobygames/ps2/genres/trading & collectable card game.json'
+import tricksAndStunts from '../Data/mobygames/ps2/genres/tricks & stunts.json'
+import triviaQuiz from '../Data/mobygames/ps2/genres/trivia & quiz.json'
+import turnBasedStrategy from '../Data/mobygames/ps2/genres/turn-based strategy.json'
+import vehicleSimulator from '../Data/mobygames/ps2/genres/vehicle simulator.json'
+import vehicularCombat from '../Data/mobygames/ps2/genres/vehicular combat.json'
+import visualNovel from '../Data/mobygames/ps2/genres/visual novel.json'
+import voiceControl from '../Data/mobygames/ps2/genres/voice control.json'
+import war from '../Data/mobygames/ps2/genres/war.json'
+import worldWarII from '../Data/mobygames/ps2/genres/world war ii.json'
+import wrestling from '../Data/mobygames/ps2/genres/wrestling.json'
+import xtreme from '../Data/mobygames/ps2/genres/xtreme & oddball sports.json'
 
-const gameListWithInterface: GameList = { ...masterWebGameList }
+const genreList = {
+actionRPG: { name: 'Action RPG', key: 'slug', data: actionRPG },
+action: { name: 'Action', key: 'slug', data: action },
+adult: { name: 'Adult', key: 'slug', data: adult },
+adventure: { name: 'Adventure', key: 'slug', data: adventure },
+africa: { name: 'Africa', key: 'slug', data: africa },
+anime: { name: 'Anime', key: 'slug', data: anime },
+arcade: { name: 'Arcade', key: 'slug', data: arcade },
+asia: { name: 'Asia', key: 'slug', data: asia },
+baseball: { name: 'Baseball', key: 'slug', data: baseball },
+basketball: { name: 'Basketball', key: 'slug', data: basketball },
+beatEmUp: { name: 'Beat Em Up', key: 'slug', data: beatEmUp },
+bicycle: { name: 'Bicycle', key: 'slug', data: bicycle },
+boardGame: { name: 'Board Game', key: 'slug', data: boardGame },
+boat: { name: 'Boat', key: 'slug', data: boat },
+bowling: { name: 'Bowling', key: 'slug', data: bowling },
+boxing: { name: 'Boxing', key: 'slug', data: boxing },
+businessSim: { name: 'Business Sim', key: 'slug', data: businessSim },
+cardAndTile: { name: 'Card and Tile', key: 'slug', data: cardAndTile },
+coldWar: { name: 'Cold War', key: 'slug', data: coldWar },
+comedy: { name: 'Comedy', key: 'slug', data: comedy },
+compilation: { name: 'Compilation', key: 'slug', data: compilation },
+cricket: { name: 'Cricket', key: 'slug', data: cricket },
+crime: { name: 'Crime', key: 'slug', data: crime },
+cyberpunk: { name: 'Cyberpunk', key: 'slug', data: cyberpunk },
+datingSimulation: { name: 'Dating Sim', key: 'slug', data: datingSimulation },
+detectiveAndMystery: { name: 'Detective and Mystery', key: 'slug', data: detectiveAndMystery },
+educational: { name: 'Educational', key: 'slug', data: educational },
+europe: { name: 'Europe', key: 'slug', data: europe },
+fallingBlockPuzzle: { name: 'Falling Block Puzzle', key: 'slug', data: fallingBlockPuzzle },
+fantasy: { name: 'Fantasy', key: 'slug', data: fantasy },
+fighting: { name: 'Fighting', key: 'slug', data: fighting },
+firstPerson: { name: 'First Person', key: 'slug', data: firstPerson },
+fishing: { name: 'Fishing', key: 'slug', data: fishing },
+flight: { name: 'Flight', key: 'slug', data: flight },
+football: { name: 'Football', key: 'slug', data: football },
+FMV: { name: 'FMV', key: 'slug', data: FMV },
+soccer: { name: 'Soccer', key: 'slug', data: soccer },
+gambling: { name: 'Gambling', key: 'slug', data: gambling },
+giantRobot: { name: 'Giant Robot', key: 'slug', data: giantRobot },
+golf: { name: 'Golf', key: 'slug', data: golf },
+graphicAdventure: { name: 'Graphic Adventure', key: 'slug', data: graphicAdventure },
+hackAndSlash: { name: 'Hack and Slash', key: 'slug', data: hackAndSlash },
+helicopter: { name: 'Helicopter', key: 'slug', data: helicopter },
+historical: { name: 'Historical', key: 'slug', data: historical },
+hockey: { name: 'Hockey', key: 'slug', data: hockey },
+horror: { name: 'Horror', key: 'slug', data: horror },
+horse: { name: 'Horse Racing', key: 'slug', data: horse },
+hovercraft: { name: 'Hovercraft', key: 'slug', data: hovercraft },
+hunting: { name: 'Hunting', key: 'slug', data: hunting },
+japanese: { name: 'Japanese', key: 'slug', data: japanese },
+lifeSim: { name: 'Life Sim', key: 'slug', data: lifeSim },
+martialArts: { name: 'Martial Arts', key: 'slug', data: martialArts },
+meditation: { name: 'Meditation', key: 'slug', data: meditation },
+middleEast: { name: 'Middle East', key: 'slug', data: middleEast },
+miniGames: { name: 'Minigames', key: 'slug', data: miniGames },
+motorcycle: { name: 'Motorcycle', key: 'slug', data: motorcycle },
+musicRhythm: { name: 'Music and Rhythm', key: 'slug', data: musicRhythm },
+northAmerica: { name: 'North America', key: 'slug', data: northAmerica },
+oceania: { name: 'Oceania', key: 'slug', data: oceania },
+offRoading: { name: 'Off-roading', key: 'slug', data: offRoading },
+olympicAndMixedSports: { name: 'Olympic and Mixed Sports', key: 'slug', data: olympicAndMixedSports },
+paddleAndPong: { name: 'Paddle and Pong', key: 'slug', data: paddleAndPong },
+partyGame: { name: 'Party Game', key: 'slug', data: partyGame },
+pirate: { name: 'Pirate', key: 'slug', data: pirate },
+platform: { name: 'Platform', key: 'slug', data: platform },
+pointAndSelect: { name: 'Point and Select', key: 'slug', data: pointAndSelect },
+pool: { name: 'Pool', key: 'slug', data: pool },
+postApocalyptic: { name: 'Post-Apocalyptic', key: 'slug', data: postApocalyptic },
+puzzleElements: { name: 'Puzzle Elements', key: 'slug', data: puzzleElements },
+Puzzle: { name: 'Puzzle', key: 'slug', data: Puzzle },
+quickTimeEvents: { name: 'Quick Time Events', key: 'slug', data: quickTimeEvents },
+racingAndDriving: { name: 'Racing and Driving', key: 'slug', data: racingAndDriving },
+railShooter: { name: 'Rail Shooter', key: 'slug', data: railShooter },
+realTimeStrategy: { name: 'Real Time Strategy', key: 'slug', data: realTimeStrategy },
+romance: { name: 'Romance', key: 'slug', data: romance },
+RPGElements: { name: 'RPG Elements', key: 'slug', data: RPGElements },
+RPG: { name: 'RPG', key: 'slug', data: RPG },
+rugby: { name: 'Rugby', key: 'slug', data: rugby },
+sandboxAndOpenWorld: { name: 'Sandbox and Open World', key: 'slug', data: sandboxAndOpenWorld },
+sciFi: { name: 'Sci-Fi', key: 'slug', data: sciFi },
+shooter: { name: 'Shooter', key: 'slug', data: shooter },
+simulation: { name: 'Simulation', key: 'slug', data: simulation },
+skateboarding: { name: 'Skateboarding', key: 'slug', data: skateboarding },
+spaceFlight: { name: 'Space Flight', key: 'slug', data: spaceFlight },
+specificTimePeriod: { name: 'Specific Time Period', key: 'slug', data: specificTimePeriod },
+sports: { name: 'Sports', key: 'slug', data: sports },
+spy: { name: 'Spy', key: 'slug', data: spy },
+stealth: { name: 'Stealth', key: 'slug', data: stealth },
+steampunk: { name: 'Steampunk', key: 'slug', data: steampunk },
+strategy: { name: 'Strategy', key: 'slug', data: strategy },
+streetRacing: { name: 'Street Racing', key: 'slug', data: streetRacing },
+survivalHorror: { name: 'Survival Horror', key: 'slug', data: survivalHorror },
+tacticalRPG: { name: 'Tactical RPG', key: 'slug', data: tacticalRPG },
+tacticalShooter: { name: 'Tactical Shooter', key: 'slug', data: tacticalShooter },
+tank: { name: 'Tank', key: 'slug', data: tank },
+tennis: { name: 'Tennis', key: 'slug', data: tennis },
+text: { name: 'Text', key: 'slug', data: text },
+thirdPerson: { name: 'Third Person', key: 'slug', data: thirdPerson },
+tileMatchingPuzzle: { name: 'Tile Matching Puzzle', key: 'slug', data: tileMatchingPuzzle },
+trackRacing: { name: 'Track Racing', key: 'slug', data: trackRacing },
+tradingAndCollectible: { name: 'Trading and Collectible', key: 'slug', data: tradingAndCollectible },
+tricksAndStunts: { name: 'Tricks and Stunts', key: 'slug', data: tricksAndStunts },
+triviaQuiz: { name: 'Trivia and Quiz', key: 'slug', data: triviaQuiz },
+turnBasedStrategy: { name: 'Turn Based Strategy', key: 'slug', data: turnBasedStrategy },
+vehicleSimulator: { name: 'Vehicle Simulator', key: 'slug', data: vehicleSimulator },
+vehicularCombat: { name: 'Vehicular Combat', key: 'slug', data: vehicularCombat },
+visualNovel: { name: 'Visual Novel', key: 'slug', data: visualNovel },
+war: { name: 'War', key: 'slug', data: war },
+worldWarII: { name: 'World War II', key: 'slug', data: worldWarII },
+wrestling: { name: 'Wrestling', key: 'slug', data: wrestling },
+xtreme: { name: 'Extreme Sports', key: 'slug', data: xtreme },
+}
